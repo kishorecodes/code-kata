33821da2abdde5d88c4a6416a54cfedb01a979b5
Kishore S <30261988+kishorecodes@users.noreply.github.com>
2019-09-24 14:20:20 +0530

    odd factors of number

---
+++ mathematics/mathematics19.py
@@ -0,0 +1,10 @@
+N = int(input())
+if(N<=1000):
+    lis1 = []
+    lis2 = []
+    for i in range(1,N+1):
+        if(N % i == 0):
+            if(i % 2 != 0):
+                lis1.append(i)
+    for j in lis1:
+        print(j,end=' ')
